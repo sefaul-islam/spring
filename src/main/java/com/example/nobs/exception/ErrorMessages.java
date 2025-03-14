991833f7dfa9c6e980413a9b247378e9873b3ded
sefaul-islam <sefaulislam.off@gmail.com>
2025-03-14 20:06:01 +0600

    updated exception

--- src/main/java/com/example/nobs/exception/ErrorMessages.java
+++ src/main/java/com/example/nobs/exception/ErrorMessages.java
@@ -1,7 +1,11 @@
 package com.example.nobs.exception;
 
 public enum ErrorMessages {
-    PRODUCT_NOT_FOUND("Product Not Found.");
+    PRODUCT_NOT_FOUND("Product Not Found."),
+    NAME_REQUIRED("Name is required"),
+    DESCRIPTION_LENGTH("Description must be atleast 20 characters"),
+    PRICE_CANNOT_BE_INVALIDE("Invalide Price");
+
     private final String message;
 
     ErrorMessages(String message) {

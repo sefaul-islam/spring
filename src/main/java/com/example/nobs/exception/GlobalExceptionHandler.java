189255313bd7937973fca1936c2a218468445a74
sefaul-islam <sefaulislam.off@gmail.com>
2025-03-15 22:16:51 +0600

    latest entry

--- src/main/java/com/example/nobs/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/nobs/exception/GlobalExceptionHandler.java
@@ -5,13 +5,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
 
     @ExceptionHandler(ProductNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleProductNotFound(ProductNotFoundException exception){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(exception.getMessage()));
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleProductNotFound(ProductNotFoundException exception){
+        return new ErrorResponse(exception.getMessage());
     }
+
+    @ExceptionHandler(ProductNotValidException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleProductNotValidException(ProductNotValidException exception){
+        return new ErrorResponse(exception.getMessage());
+    }
+
 }
--- src/main/java/com/example/nobs/product/services/CreateProductService.java
+++ src/main/java/com/example/nobs/product/services/CreateProductService.java
@@ -1,9 +1,12 @@
 package com.example.nobs.product.services;
 
+import com.example.nobs.exception.ErrorMessages;
+import com.example.nobs.exception.ProductNotValidException;
 import com.example.nobs.product.Command;
 import com.example.nobs.product.model.Product;
 import com.example.nobs.product.model.ProductDTO;
 import com.example.nobs.product.ProductRepository;
+import io.micrometer.common.util.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -18,8 +21,25 @@ public class CreateProductService implements Command<Product, ProductDTO> {
 
     @Override
     public ResponseEntity<ProductDTO> execute(Product product) {
-       Product savedproduct= productrepository.save(product);
+
+        validateProduct(product);
+
+
+        Product savedproduct= productrepository.save(product);
         return ResponseEntity.status(HttpStatus.CREATED).body(new ProductDTO(savedproduct));
+    }
+
+    private static void validateProduct(Product product) {
+        if(StringUtils.isEmpty(product.getName())){
+            throw new ProductNotValidException(ErrorMessages.NAME_REQUIRED.getMessage());
+        }
+        if(product.getDescription().length() < 20){
+            throw new ProductNotValidException(ErrorMessages.DESCRIPTION_LENGTH.getMessage());
+        }
+
+        if(product.getPrice() == null || product.getPrice()<0.00){
+            throw new ProductNotValidException(ErrorMessages.PRICE_CANNOT_BE_INVALIDE.getMessage());
+        }
     }
 
 }

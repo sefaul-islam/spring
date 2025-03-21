e53af88edd629a111246870d73adc7d6e11fa00c
sefaul-islam <sefaulislam.off@gmail.com>
2025-03-21 23:26:25 +0600

    adding search custom jpql query

--- src/main/java/com/example/nobs/product/ProductRepository.java
+++ src/main/java/com/example/nobs/product/ProductRepository.java
@@ -2,6 +2,8 @@ package com.example.nobs.product;
 
 import com.example.nobs.product.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +13,6 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product,Integer> {
 
      List<Product> findByNameContaining(String name);
+     @Query("SELECT p FROM Product p Where p.name LIKE %:keyword% OR p.description LIKE %:keyword%")
+     List<Product> findByNameOrDescriptionContaining(@Param("keyword") String string);
 }

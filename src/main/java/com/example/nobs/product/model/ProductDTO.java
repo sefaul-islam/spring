a705e597235184b6dbb6180e6dbaba63afe4c060
sefaul-islam <sefaulislam.off@gmail.com>
2025-03-29 22:49:42 +0600

    error detection for security

--- src/main/java/com/example/nobs/product/model/ProductDTO.java
+++ src/main/java/com/example/nobs/product/model/ProductDTO.java
@@ -14,5 +14,27 @@ public class ProductDTO {
         this.description = product.getDescription();
     }
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
